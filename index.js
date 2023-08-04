89eb82d9d12e8a644072ef138d44744a98b7e19f
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-08-04 22:32:51 +0500

    basics of JS

---
+++ index.js
@@ -0,0 +1,30 @@
+// let firstName = "zarnain"
+// let lastName = "naz"
+// let fullName = `I want to become a ${firstName} ${lastName}`
+// console.log(fullName)
+
+// console.log(firstName.toUpperCase())
+// console.log(lastName.indexOf("a"))
+
+// let subjects ="english ,urdu ,math"
+// console.log(subjects.lastIndexOf("urdu"))
+// let result = subjects.trim()
+// console.log(result.lastIndexOf("urdu"))
+
+//check include
+// console.log(result.includes("english"))//true
+// console.log(result.includes("English"))//false
+
+//slice method
+// let firstName = "helloZarnain";
+// let res = firstName.slice(0,3) // it gives hel
+// console.log("original string " + firstName)
+// console.log("after slice " +res)
+
+//split method
+// let names="zarnain alina maham neha? "
+// let myNames = names.split(" ")
+// console.log(myNames)
+
+//JS strings imutable
+
