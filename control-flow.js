d5cff19261f63124b363ae84c621ad32f4aeb975
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-08-05 02:40:25 +0500

    A JS BASICS

---
+++ control-flow.js
@@ -0,0 +1,23 @@
+// for loops
+//CONTROL FLOW
+let name = ["zaryoon " ,"zara","ahmed"]
+// console.log(name[0])
+// for(let i=0 ; i<name.length;i++){
+//     console.log(name[i])
+// }
+
+// for(let i=0; i<=5; i++){
+//     console.log("for loop",i)
+// }
+// console.log("************")
+//same syntax for while loop 
+// let j=1;
+// while(j<=5){
+//     console.log("*******")
+//     j++
+// }
+let j =0;
+while(j<name.length){
+    console.log(name[j])
+    j++
+}
---
+++ data-types.js
@@ -0,0 +1,80 @@
+// let firstName = "zarnain"
+// let lastName = "naz"
+// let fullName = `I want to become a ${firstName} ${lastName}`
+// console.log(fullName)
+
+// console.log(firstName.toUpperCase())
+// console.log(lastName.indexOf("a"))
+
+// let subjects ="english ,urdu ,math"
+// console.log(subjects.lastIndexOf("urdu"))
+// let result = subjects.trim()         //remove extra spaces
+// console.log(result.lastIndexOf("urdu"))
+
+//check include
+// console.log(result.includes("english"))//true
+// console.log(result.includes("English"))//false
+
+//slice method slice(start , end ) ,(start)
+// let firstName = "helloZarnain";
+// let res = firstName.slice(0,3) // it gives hel
+// console.log("original string " + firstName)
+// console.log("after slice " +res)
+
+//split method
+let names="zarnain alina maham neha? "
+let myNames = names.split("n")
+console.log(myNames)
+
+//JS strings imutable
+// let number = "0"
+// console.log(typeof number)
+// let newNum = Number(number)
+// console.log(newNum, typeof newNum)
+
+//boolean 
+// let age = ""
+// let other = Boolean(age)
+// console.log(other)
+
+//ARRAY
+// let myarr=["karahi " ,"nosepin","makes"]
+// let newVar = myarr[1]="xyz"
+// console.log(newVar)
+// myarr[0]="macaroni"
+// console.log(myarr)
+// // let r = myarr.push("newDIsh")
+// // console.log(r)
+// let v = myarr.pop([1])
+// console.log(v)
+
+//ARRAY'S DIFFERENT METHODS
+//JOIN METHOD
+//  console.log(myarr.join(" , "))
+// console.log(myarr.indexOf("makes")) //indexOF
+
+//concat method
+// let addedArray = ["hammad " , "ali" , "mynmae"]
+// console.log(addedArray.concat(myarr))
+
+// let newDIsh  = myarr.concat(addedArray);
+// console.log(newDIsh.length)
+//length method
+// console.log(addedArray.length);
+
+//push method
+// var rr = newDIsh.push("newly pushed tem");
+// console.log(newDIsh)
+
+//pop
+// console.log(newDIsh.push("some"))
+
+//boolean values and comparison operator
+// let x =true;
+// let y= "true"
+// console.log(typeof x , typeof y)
+
+// let email = "hello@gmail.com"
+// let booleanValue =email.includes("E")
+// console.log(booleanValue)
+// console.log(email.includes("h"))
