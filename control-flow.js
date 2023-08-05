3d3f17ebdfc1bac9da84c01a80ec09e93ca94997
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-08-06 03:17:12 +0500

    if else do while loop continue

--- control-flow.js
+++ control-flow.js
@@ -1,6 +1,6 @@
 // for loops
 //CONTROL FLOW
-let name = ["zaryoon " ,"zara","ahmed"]
+// let name = ["zaryoon " ,"zara","ahmed"]
 // console.log(name[0])
 // for(let i=0 ; i<name.length;i++){
 //     console.log(name[i])
@@ -10,14 +10,43 @@ let name = ["zaryoon " ,"zara","ahmed"]
 //     console.log("for loop",i)
 // }
 // console.log("************")
+
+//while loop
 //same syntax for while loop 
 // let j=1;
 // while(j<=5){
 //     console.log("*******")
 //     j++
 // }
-let j =0;
-while(j<name.length){
-    console.log(name[j])
-    j++
-}
+// let j =0;
+// while(j<name.length){
+//     console.log(name[j])
+//     j++
+// }
+// let favDish = ["biryani" , "chat" ,"fries"]
+// let j=0;
+// while(j<favDish.length){
+//     console.log("my dish is " +favDish[j])
+//     j++;
+// }
+//same code for "for loop"
+// for(let i=0; i<favDish.length;i++){
+//     console.log("dish is "+favDish[i])
+// }
+
+//do while loop
+// let j=1;
+// do{
+// console.log("run please",j);
+// j++
+// }
+// while(j<=1)
+
+// if else ,else if
+//continue and break statement
+ for(let i=0; i<=10;i++){
+    if(i==2){
+        continue;
+     } console.log("value is" , i)
+    
+ }
