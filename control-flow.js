d9b344f7a40f383db9ba095953d53da13f385542
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-08-07 01:49:12 +0500

    switch ternary operators truthy falsy values

--- control-flow.js
+++ control-flow.js
@@ -11,6 +11,13 @@
 // }
 // console.log("************")
 
+// var array =[1,2,3]
+// for (let index = 0; index < array.length; index++) {
+//     const elem = array[index];
+//     console.log(elem)
+    
+// }
+
 //while loop
 //same syntax for while loop 
 // let j=1;
@@ -43,13 +50,23 @@
 // while(j<=1)
 
 // if else ,else if
+
+
 //continue and break statement
 //  for(let i=0; i<=10;i++){
 //     if(i==2){
 //         continue;
 //      } console.log("value is" , i)
-    
 //  }
+
+// let text =""
+// for (let index = 0; index < 10; index++) {
+//     if(index===3){
+//         continue  //skip this iteration print 012456789
+//     }
+//     text+=index  
+// }
+// console.log(text);
 
 //break
 // for(let i=1; i<=10; i++){
@@ -62,9 +79,43 @@
 // console.log("we are out ");
 
 //operators
-let status = false;
-if(!status){
-    console.log(status);
-}else{
-    console.log("hee"); 
-}
+// let status = false;
+// if(!status){
+//     console.log(status);
+// }else{
+//     console.log("hee"); 
+// }
+
+//ternary operator
+// let age=20;
+// let weight = 3;
+// let result = age>18? weight>20? "yes" : "no":"no problem"
+// console.log(result);
+
+//swtich 
+// let age=112;
+// switch (age) {
+//     case 10:
+//         result="hello"
+//         break;
+//         case 11:
+//             result="i am 11"
+//             break;
+//     default:
+//         result = "outer "
+// }
+// console.log(result)
+
+// let code ="US"
+// switch (code) {
+//     case "in":
+//         console.log("india")
+//         break;
+// case "US":
+//     console.log("US")
+//     break
+//     case "PK":
+//         console.log("PK")
+//     default:
+//        console.log("outer");
+// }
