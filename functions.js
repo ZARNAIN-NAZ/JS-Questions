61ce595c2789bae659536a1526fcb8973595ea24
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-08-07 22:05:30 +0500

    FUNCTIONS

---
+++ functions.js
@@ -0,0 +1,27 @@
+//functions
+
+// let fun=function(name , fname){
+//     console.log("my name is " ,name ,"my father name is " ,fname);
+
+// }
+
+// fun("zarnain" , "abid");
+// fun("harry")
+
+// let age = function(birthYear){
+//     let myage= 2023-birthYear;
+//     console.log(myage);
+// }
+// age(2004)
+
+//same for return 
+// let age = function(birthYear ,currentYear=2023){
+//     let myage = currentYear - birthYear;
+//     return myage
+// }
+// // console.log(age(2004));
+// // let final = age(2004)
+// // console.log(final)
+
+//ARROW FUNCTION
+  
