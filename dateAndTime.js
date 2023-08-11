1c7e604803c0a26192e2a4995e0822988272ce5f
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-08-11 11:32:16 +0500

    get date ND TIME METHODS

---
+++ dateAndTime.js
@@ -0,0 +1,34 @@
+// let currentDate= new Date(11,8,23,11,23,3)
+// console.log(currentDate);
+
+//getTime in ms
+// let currentDate2 = new Date().getTime()
+// console.log(currentDate2);
+
+// Date and Time methods 
+let completeDT =new Date()
+// console.log(completeDT);
+
+//getFull Year
+// console.log(completeDT.getFullYear());
+
+//get Month
+// console.log(completeDT.getMonth());//works at index 0-11
+
+// Week Day 
+// console.log(completeDT.getDay());//0-6 0-sunday 1-monday
+
+//Month Date
+// console.log(completeDT.getDate());
+
+//get Hours
+// console.log(completeDT.getHours());
+
+// get minutes
+// console.log(completeDT.getMinutes());
+
+//get seconds
+// console.log(completeDT.getSeconds());
+
+//digital clock
+
